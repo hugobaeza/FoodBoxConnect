178caaa873f182acc4622568c88e5e191672c813
hugobaeza <victorhugobaezarocha@gmail.com>
2025-03-20 21:43:10 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,8 +9,11 @@ pip install sqlparse==0.2.4
 # Instala las demás dependencias
 pip install -r requirements.txt
 
+# Crear estructura de carpetas para archivos estáticos
+mkdir -p web/static/css web/static/js web/static/images
+
 # Ejecuta las migraciones
 python manage.py migrate
 
 # Colecta los archivos estáticos
-python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input --noinput
